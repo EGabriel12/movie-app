ed2c0f8641100422dd284fe4271d240025b865a1
Elias Gabriel <elias.gabrielfn12@gmail.com>
2023-02-03 23:11:54 -0400

    Feat: remove storyboard

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Elias Gabriel on 03/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
